f0ed5a32104903d86e0d5ff9508695ca6ea02559
DMiniKim <angks1212@naver.com>
2025-08-04 15:46:19 +0900

    (2025.8.4) 시간 복잡도 & 공간 복잡도

---
+++ Class 02th ( Brute Force )/Source.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+#pragma region 완전 탐색
+    // 가능한 모든 경우의 수를 탐색하면
+    // 결과를 찾아내는 알고리즘.
+    int passWord[4] = { 5,5,3,6 };
+    int searchArr[4] = { 0,0,0,0 };
+    int count = 0;
+    while ((passWord[0] != searchArr[0]) || (passWord[1] != searchArr[1]) || (passWord[2] != searchArr[2]) || (passWord[3] != searchArr[3]))
+    {
+        searchArr[3]++;
+        if (searchArr[3] == 10)
+        {
+            searchArr[3] = 0;
+            searchArr[2]++;
+        }
+        if (searchArr[2] == 10)
+        {
+            searchArr[2] = 0;
+            searchArr[1]++;
+        }
+        if (searchArr[1] == 10)
+        {
+            searchArr[1] = 0;
+            searchArr[0]++;
+        }
+
+        count++;
+    }
+
+    cout << "자물쇠를 풀기 위해 " << count << " 만큼 뻘 짓을 했습니다 " << endl;
+#pragma endregion
+
+    return 0;
+}
+
+
---
+++ Program/Program.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+#pragma region 거품 정렬
+	// 서로 인접한 두 원소를 비교하여 정렬하는 알고리즘입니다.
+	//int arr[]{ 6,4,2,7,9 };
+	//int index = 0;
+	//int i = 0;
+	//for (int j = 0; j < 4; j++)
+	//{
+	//
+	//	for (int i = 0; i < 4; i++)
+	//	{
+	//
+	//		if (arr[i] < arr[i + 1]) continue;
+	//		else if (arr[i] > arr[i + 1])
+	//		{
+	//			int temp = arr[i];
+	//			arr[i] = arr[i + 1];
+	//			arr[i + 1] = temp;
+	//		}
+	//
+	//		if (arr[0] < arr[1])
+	//		{
+	//			if (arr[1] < arr[2])
+	//			{
+	//				if (arr[2] < arr[3])
+	//				{
+	//					if (arr[3] < arr[4])
+	//					{
+	//						break;
+	//					}
+	//				}
+	//			}
+	//		}
+	//		index++;
+	//	}
+	//}
+	int list[] = { 5,3,1,4,7 };
+	int size = sizeof(list) / sizeof(list[0]);
+
+	for (int  i = 0; i < size-1; i++)
+	{
+		for (int j = 0; j < (size-i); j++)
+		{
+			if (list[j]> list[j+1])
+			{
+				std::swap(list[j], list[j + 1]);
+			}
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << list[i] <<" ";
+	}
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	cout << arr[i] << " ";
+	//}
+	//cout << index << endl;
+
+
+#pragma endregion
+
+	return 0;
+}
+
+
