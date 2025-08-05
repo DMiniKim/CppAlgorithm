45b6c512a9c89e2c4f7aa9731d817c5f4733d5d9
DMiniKim <angks1212@naver.com>
2025-08-05 15:41:59 +0900

    (2025.8.5) 거품 정렬

---
+++ Class 04th ( Select Sort )/Source.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+#pragma region 선택 정렬
+	// 주어진 리스트 중에 최소값을 찾은 다음 특정한 위치에서
+	// 결과를 교체하는 방식으로 정렬하는 알고리즘입니다.
+	int arr[] = { 13, 44, 7, 21, 14, 17, 20, 6, 37, 22, 26, 10, 3, 8, 33, 16, 24, 1, 34, 39, 18, 12, 42, 35, 38, 25, 31, 32, 28, 27 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int min_index = 999999999;
+	for (int i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[j] < arr[min_index])
+			{
+				min_index = j;
+			}
+		}
+
+		if (min_index != i) // 최소 index의 위치가 i의 위치가 아니라면 
+		{
+			std::swap(arr[min_index], arr[i]);
+		}
+
+	}
+
+
+	for (const auto element : arr)
+	{
+		cout << element << " ";
+	}
+
+
+#pragma endregion
+
+	return 0;
+}
+
+
---
+++ Class 05th ( Insert Sort )/Source.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 삽입 정렬
+	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
+	// 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘입니다.
+	int arr[] = { 18, 3, 5, 44, 39, 28, 33, 27, 14, 30 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int key;
+	for (int i = 1; i < size; i++) // key는 1번인덱스부터 해야해서 i = 1
+	{
+		key = arr[i];
+		for (int j = i - 1; j >= 0; j--) // key보다 -1 부터 역순으로 써치하기 위해
+		{								// j = i-1
+			if (key < arr[j])			// key보다 이전 인덱스가 크다면
+			{
+				arr[j + 1] = arr[j];
+			}
+			else { break; }
+
+			arr[j] = key; // 외부에 j를 선언해서  for 문밖으로빼서	
+			// 연산을 줄이는 게 가능
+		}
+	}
+
+	for (const auto& element : arr)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
+
+
